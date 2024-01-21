a59bb8a5aa52b08b4fdcd6c4995c259607f3bc3e
Ben Shewan <benbshewan@gmail.com>
2024-01-21 19:11:52 -0400

    messing with cards and styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,18 @@
 	import '../app.css';
 </script>
 
-<nav>
-	<a href="/">home</a>
-	<a href="/apps">apps</a>
-</nav>
+<div class="flex h-screen flex-col">
+	<header class="bg-blue-950 py-4 text-white">
+		<div class="container mx-auto flex items-center justify-between px-4">
+			<h1 class="text-2xl">BenFlex</h1>
+			<nav>
+				<a href="/" class="px-1 text-xl text-white hover:text-gray-300">Home</a>
+				<a href="/apps" class="px-1 text-xl text-white hover:text-gray-300">Apps</a>
+			</nav>
+		</div>
+	</header>
 
-<slot />
+	<div class="flex flex-grow bg-gradient-to-tr from-purple-900 to-blue-900">
+		<slot />
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,27 @@
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<script>
+	import Fa from 'svelte-fa';
+	import { faFilm } from '@fortawesome/free-solid-svg-icons';
+</script>
 
-<style lang="postcss">
-	:global(html) {
-		background-color: theme(colors.gray.100);
-	}
-</style>
+<div class="container mx-auto flex flex-col items-center">
+	<h1 class="my-6 text-3xl font-bold text-slate-200">Welcome to BenFlex</h1>
+
+	<article
+		class="mx-auto max-w-sm transform cursor-pointer rounded-md bg-black/15 shadow-md backdrop-blur-sm duration-300 hover:-translate-y-1 hover:shadow-xl"
+	>
+		<div class="max-h-140 overflow-hidden">
+			<img
+				class="h-auto w-full"
+				src="https://images.squarespace-cdn.com/content/v1/58dd442dbebafb88160c6929/1593716780236-POZ4F9TRGHIMKVVZRK7U/ke17ZwdGBToddI8pDm48kPvdHwxe8vxmYnjZvCzAU61Zw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpwxsJYjeBOStWfhZBaQGTlDOVYbSZjX976lCUHHLPdHUlrmON35exl807wLzw9VMGw/visitshire.jpg?format=800w"
+				alt=""
+			/>
+		</div>
+		<div class="my-auto p-7 pb-12">
+			<div class="flex items-center text-2xl font-semibold text-slate-200">
+				<Fa icon={faFilm} />
+				<h1 class="pl-1">Plex</h1>
+			</div>
+			<p class="mt-5 text-xl font-light leading-relaxed text-gray-400">For all your media needs</p>
+		</div>
+	</article>
+</div>
